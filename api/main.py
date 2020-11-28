41f9857eee5c6b41b6afe97f754589e9b543f39a
ruthaleks <ruth.aleks@gmail.com>
2020-11-28 16:41:04 +0100

    save uploaded file

--- api/main.py
+++ api/main.py
@@ -1,11 +1,13 @@
 import os
 
 import fastapi
+import pathlib
+import hashlib
 
 app = fastapi.FastAPI()
 
 VIDEO_ROOT = os.environ["DAS_FILEPATH"] if "DAS_FILEPATH" in os.environ else "./video_uploads"
-
+os.makedirs(VIDEO_ROOT, exist_ok=True)
 
 @app.get("/")
 async def hello():
@@ -17,10 +19,15 @@ async def upload(video: fastapi.UploadFile = fastapi.File(...)):
     await video.seek(0)
     content = await video.read()
 
-    print(content)
     print(video.filename)
     print(video.content_type)
-    open("/tmp/hello.mkv", "wb").write(content)
+
+    # save video
+    suffix = pathlib.Path(video.filename).suffix
+    file_hash = hashlib.md5(content).hexdigest()
+    filename = file_hash + suffix
+    path = os.path.join(VIDEO_ROOT, filename)
+    open(path, "wb").write(content)
 
     return {"hello": "world"}
 
